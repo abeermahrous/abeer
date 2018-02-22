46f0223d654771c1c7f9252ea5560f494dcf3b84
Abeer <31357071+abeermahrous@users.noreply.github.com>
2018-02-22 06:19:53 +0200

    Add files via upload

---
+++ control2.ino
@@ -0,0 +1,19 @@
+byte ledpin = 2;
+
+void setup() {
+
+  pinMode( ledpin, OUTPUT  );
+}
+
+void loop() {
+  for (int i=0;i<=10;i++)
+  {
+  analogWrite(ledpin, i);
+  delay(1000);
+  }
+  for(int j=0; j>=10;j--)
+  {
+  digitalWrite(ledpin, j);
+  delay(1000);
+  }
+}
